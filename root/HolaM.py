dc83a89708d7f5749b34d5e1810bbc1bf1714ee0
DiR081 <tecdiegorodriguez@gmail.com>
2020-04-26 23:56:11 -0500

    Se realiza una suma

--- root/HolaM.py
+++ root/HolaM.py
@@ -1,8 +1,14 @@
 # Hola Diro
 print('Hola Mundo')
 
+#Como no puedo enviar por ahora parametros... entonces
+
+print('Vamos a Sumar dos Números')
+
+
 def suma(a=0, b=0):
 	"""  Este es la Suma."""
 	print('Suma dos valores...')
 	return a + b
 
+print(suma(4, 5))
