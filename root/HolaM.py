ad970f843505c27051429e3a328f96ad722a3bc1
DiR081 <tecdiegorodriguez@gmail.com>
2020-04-27 14:14:54 -0500

    Se ReUbica funcion Suma - Se envia parametros a la WEB.

--- root/HolaM.py
+++ root/HolaM.py
@@ -1,5 +1,11 @@
 from wsgiref.simple_server import make_server
 
+""" 
+	Doc String:
+		Se cree que puedo mostrar la suma en la pantalla
+	
+  """
+
 HTML = """
 <!DOCTYPE html>
 <html>
@@ -8,10 +14,24 @@ HTML = """
   </head>
   <body>
     <h1>Hola gente de códigofacilito</h1>
+	<Br>
+	<table>
+		<li>
+			{{ suma(4, 8) }}
+		</li>
+	</table>
   </body>
 </html>
 """
 
+# Suma
+def suma(a=0, b=0):
+	"""  Este es la Suma. de dos valores pasado en lo parametros"""
+	print('Suma dos valores...')
+	print(str(a) + ' + ' + str(b) + ' = ')
+	return a + b
+
+# Esta función arranca el servidor WEB
 def application(environ, start_response):
     headers = [ ('Content-type', 'text/html; charset=utf-8') ]
 
@@ -28,13 +48,6 @@ server.serve_forever()
 #Como no puedo enviar por ahora parametros... entonces
 
 print('Vamos a Sumar dos Números')
-
-
-def suma(a=0, b=0):
-	"""  Este es la Suma."""
-	print('Suma dos valores...')
-	print(str(a) + ' + ' + str(b) + ' = ')
-	return a + b
 
 print(suma(4, 5))
 
