80e983055b6130ce68583ecb59db74e2ca4cb75c
DiR081 <tecdiegorodriguez@gmail.com>
2020-04-27 10:41:49 -0500

    Se copia todo el Codigo del WEB y me mantienen los dos

--- root/HolaM.py
+++ root/HolaM.py
@@ -1,3 +1,31 @@
+from wsgiref.simple_server import make_server
+
+HTML = """
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>T-Hola Mundo Dos</title>
+  </head>
+  <body>
+    <h1>Hola gente de códigofacilito</h1>
+  </body>
+</html>
+"""
+
+def application(environ, start_response):
+    headers = [ ('Content-type', 'text/html; charset=utf-8') ]
+
+    start_response('200 OK', headers)
+
+    return [bytes(HTML, 'utf-8')]
+
+server = make_server('localhost', 8000, application)
+server.serve_forever()
+
+
+#
+
+
 # Hola Diro
 print('Hola Mundo')
 
